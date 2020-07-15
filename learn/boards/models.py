3aa95c975325fcced67486198f55da966dc4e3b7
RmarzooqUR <marzooq697231@gmail.com>
2020-07-15 23:39:43 +0530

    add __str__ to models

--- learn/boards/models.py
+++ learn/boards/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 # Create your models here.
 class Boards(models.Model):
     name = models.CharField(max_length=50,unique=True)
     description = models.CharField(max_length=255)
+    
     def __str__(self):
         return self.name
 
@@ -15,6 +17,10 @@ class Topics(models.Model):
     board = models.ForeignKey(Boards,on_delete=models.CASCADE)
     starter = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+    
+
 class Post(models.Model):
     msg = models.TextField(max_length=1000)
     description = models.CharField(max_length=255,default='default')
@@ -23,3 +29,7 @@ class Post(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     updated_at = models.DateTimeField(null = True)
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+",null=True)
+
+    def __str__(self):
+        return Truncator(self.msg).chars(30)
+    
